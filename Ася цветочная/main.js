de64f7bac382ad28018d7d23a41d9a3a957e968c
Анастасия <133859026+asakine@users.noreply.github.com>
2023-05-17 12:08:41 +0300

    Add files via upload

---
+++ Ася цветочная/main.js
@@ -0,0 +1,8 @@
+let btn1 = document.querySelector('.knopki');
+btn1.onclick = function(){
+    document.getElementById('kn').classList.toggle('knopki1');
+}
+let btn2 = document.querySelector('.knopki');
+btn2.onclick = function(){
+    document.getElementById('kn1').classList.toggle('knopki1');
+}
